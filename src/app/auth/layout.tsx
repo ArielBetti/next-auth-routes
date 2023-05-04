5a2f651f971803e390e86916727d4392d0054363
ArielBetti <ariel.betti@gmail.com>
2023-05-04 04:27:12 -0300

    feat: create auth layout

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+import type { ReactNode } from "react";
+
+// next
+import { useRouter } from "next/navigation";
+
+// ::
+export default function Layout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <>
+      <button
+        className="decoration-2 decoration-blue-500 mb-5 font-thin text-sm border-b-2 border-b-red-500"
+        onClick={() => router.back()}
+      >
+        Go back page
+      </button>{" "}
+      {children}
+    </>
+  );
+}
