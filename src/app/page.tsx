6c9b0f563f97fb322bc20945c2d01957bf5a4981
ArielBetti <ariel.betti@gmail.com>
2023-05-04 04:47:48 -0300

    feat: add show me the code button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import CardRoutes from "./components/CardRoutes";
 
 // ::
 export default function IndexPage() {
   return (
     <div>
-      <div className="flex flex-col gap-2 mb-10 prose">
+      <div className="flex flex-col gap-2 mb-5 prose">
         <h1 className="m-0">👻 Welcome!</h1>
         <p>
           This is an example site to demonstrate how to use{" "}
@@ -16,6 +17,15 @@ export default function IndexPage() {
           </a>{" "}
           for authentication routes.
         </p>
+      </div>
+      <div className="max-w-[200px] mt-5 mb-10 ">
+        <Link
+          target="_blank"
+          className="font-semibold flex items-center justify-center gap-2 p-2 rounded-md border-b-4 text-black bg-yellow-400 border-b-yellow-600"
+          href="https://github.com/ArielBetti/next-auth-routes"
+        >
+          💾 Show me the code
+        </Link>
       </div>
       {/** @ts-expect-error */}
       <CardRoutes />
